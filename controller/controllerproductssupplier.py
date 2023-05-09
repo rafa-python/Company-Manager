45f7308705f8cd0618787222900b19c263b6a752
rafa-python <rafadeveloper10@gmail.com>
2023-05-09 11:51:21 -0300

    criei a classe ControllerProductsSupplier

--- controller/controllerproductssupplier.py
+++ controller/controllerproductssupplier.py
@@ -3,3 +3,6 @@ import sys
 from prettytable import PrettyTable
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from dao.daoclasses import *
+
+class ControllerProductsSupplier:
+    pass
